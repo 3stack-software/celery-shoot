837856fa8c746edede278300890d773624852d42
Nathan Muir <nathan.muir@3stack.com>
2025-04-14 16:08:59 +1200

    feat: update celery & python to latest

--- test/tasks.py
+++ test/tasks.py
@@ -3,46 +3,46 @@ import time
 
 from celery import Celery
 
-broker = os.environ.get('AMQP_HOST', 'amqp://guest:guest@localhost//')
+broker = os.environ.get("AMQP_HOST", "amqp://guest:guest@localhost//")
 
-celery = Celery('tasks', broker=broker,  backend='rpc://')
+app = Celery("tasks", broker=broker, backend="rpc://")
 
 
-@celery.task
+@app.task
 def add(x, y):
-    print('got task to add {} + {} = {}'.format(x, y, x+y))
+    print(f"got task to add {x} + {y} = {x+y}")
     return x + y
 
 
-@celery.task
+@app.task
 def sleep(x):
     time.sleep(x)
     return x
 
 
-@celery.task
+@app.task
 def curtime():
     current_time = int(time.time() * 1000)
-    print('the time is {}'.format(current_time))
-    print('the time is {}'.format(time.time()))
+    print(f"the time is {current_time}")
+    print(f"the time is {time.time()}")
     return current_time
 
 
-@celery.task
+@app.task
 def error(msg):
     raise Exception(msg)
 
 
-@celery.task
+@app.task
 def echo(msg):
     return msg
 
 
 # client should call with ignoreResult=True as results are never sent
-@celery.task(ignore_result=True)
-def send_email(to='me@example.com', title='hi'):
+@app.task(ignore_result=True)
+def send_email(to="me@example.com", title="hi"):
     print("Sending email to '%s' with title '%s'" % (to, title))
 
 
 if __name__ == "__main__":
-    celery.start()
+    app.worker_main(argv=["worker", "--loglevel=INFO"])
